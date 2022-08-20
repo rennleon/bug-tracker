1784d7d3a387f0868d661940a265550ea0c1b04d
rennleon <renleonz@gmail.com>
2022-08-20 16:37:52 -0500

    Refactor controller

--- backend/controllers/staffTicketsController.js
+++ backend/controllers/staffTicketsController.js
@@ -3,43 +3,53 @@ const Ticket = require("../models/Ticket");
 const TICKET_STATUS = require("../config/ticketStatusConstants");
 
 const geSttaffTickets = async (req = request, res = response) => {
-  const tickets = await Ticket.find({ staffId: req.userId }).exec();
-  if (!tickets || tickets.length === 0) return res.sendStatus(204);
+  try {
+    const tickets = await Ticket.find({ staffId: req.userId }).exec();
+    if (!tickets || tickets.length === 0) return res.sendStatus(204);
 
-  res.json(tickets);
+    res.json(tickets);
+  } catch (err) {
+    res.status(500).json({ meesage: err.message });
+  }
 };
 
 const takeTicket = async (req = request, res = response) => {
-  const foundTicket = await Ticket.findOne({
-    _id: req.params.id,
-    userId: req.userId,
-  }).exec();
-  if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
+  try {
+    const foundTicket = await Ticket.findById(req.params.id).exec();
+    if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
 
-  if (foundTicket.status !== TICKET_STATUS.UNASSIGNED)
-    return res.sendStatus(409);
+    if (foundTicket.status !== TICKET_STATUS.UNASSIGNED)
+      return res.sendStatus(409);
 
-  foundTicket.staffId = userId;
-  foundTicket.status = TICKET_STATUS.ASSIGNED;
-  await foundTicket.save();
+    foundTicket.staffId = req.userId;
+    foundTicket.status = TICKET_STATUS.ASSIGNED;
+    await foundTicket.save();
 
-  res.sendStatus(200);
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).json({ meesage: err.message });
+  }
 };
 
 const closeTicket = async (req = request, res = response) => {
-  const foundTicket = await Ticket.findOne({
-    _id: req.params.id,
-    userId: req.userId,
-  }).exec();
-  if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
+  try {
+    const foundTicket = await Ticket.findOne({
+      _id: req.params.id,
+      staffId: req.userId,
+    }).exec();
+    if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
 
-  if (foundTicket.status !== TICKET_STATUS.ASSIGNED) return res.sendStatus(409);
+    if (foundTicket.status !== TICKET_STATUS.ASSIGNED)
+      return res.sendStatus(409);
 
-  foundTicket.status = TICKET_STATUS.CLOSED;
-  foundTicket.closedAt = new Date().toISOString();
-  await foundTicket.save();
+    foundTicket.status = TICKET_STATUS.CLOSED;
+    foundTicket.closedAt = new Date().toISOString();
+    await foundTicket.save();
 
-  res.json(ticket);
+    res.json(ticket);
+  } catch (err) {
+    res.status(500).json({ meesage: err.message });
+  }
 };
 
 module.exports = {
