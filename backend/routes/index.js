dac36de832cc6e08fdb32c4f49bd2d47415a422c
rennleon <renleonz@gmail.com>
2022-08-20 16:03:33 -0500

    Add route for staff tickets

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userRoutes = require("./userRoutes");
 const ticketRoutes = require("./ticketRoutes");
 const userTicketRoutes = require("./userTicketRoutes");
+const staffTicketRoutes = require("./staffTicketRoutes");
 
 router.use("/users", userRoutes);
 router.use("/users", userTicketRoutes);
+router.use("/staff", staffTicketRoutes);
 router.use("/tickets", ticketRoutes);
 
 module.exports = router;
