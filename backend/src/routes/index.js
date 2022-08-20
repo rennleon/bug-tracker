be159f234915747656e01e134719267c650d577f
rennleon <renleonz@gmail.com>
2022-08-20 18:04:46 -0500

    Setup routes for login

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const loginRoutes = require("./loginRoutes");
+
 const userRoutes = require("./userRoutes");
 const ticketRoutes = require("./ticketRoutes");
 const userTicketRoutes = require("./userTicketRoutes");
 const staffTicketRoutes = require("./staffTicketRoutes");
+
+router.use("/login", loginRoutes);
 
 router.use("/users", userRoutes);
 router.use("/users", userTicketRoutes);
