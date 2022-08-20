363cf4cdcc0705f8664054171d47bc1b1238ae9f
rennleon <renleonz@gmail.com>
2022-08-20 15:59:07 -0500

    Create middleware to check for user id

---
+++ backend/middleware/verifyStaffId.js
@@ -0,0 +1,14 @@
+const { request, response } = require("express");
+const User = require('../models/User')
+
+const verifyStaffId = (req = request, res = response, next) => {
+  // VERIFY LATER AGAINST LOGGED IN USER WITH STAFF ROLE AND ID THE SAME AS "staffId"
+
+  const staff = await User.findById(req.params.staffId).exec();
+  if (!staff) return res.sendStatus(404);
+
+  req.userId = staff._id
+  next()
+};
+
+module.exports = { verifyStaffId };
