c74a4aae0acb44f3984338673080f497cf0d3488
rennleon <renleonz@gmail.com>
2022-08-20 16:07:31 -0500

    Add conditionals to check for ticket conflicts

--- backend/controllers/staffTicketsController.js
+++ backend/controllers/staffTicketsController.js
@@ -16,6 +16,9 @@ const takeTicket = async (req = request, res = response) => {
   }).exec();
   if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
 
+  if (foundTicket.status !== TICKET_STATUS.UNASSIGNED)
+    return res.sendStatus(409);
+
   foundTicket.staffId = userId;
   foundTicket.status = TICKET_STATUS.ASSIGNED;
   await foundTicket.save();
@@ -29,6 +32,8 @@ const closeTicket = async (req = request, res = response) => {
     userId: req.userId,
   }).exec();
   if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
+
+  if (foundTicket.status !== TICKET_STATUS.ASSIGNED) return res.sendStatus(409);
 
   foundTicket.status = TICKET_STATUS.CLOSED;
   foundTicket.closedAt = new Date().toISOString();
