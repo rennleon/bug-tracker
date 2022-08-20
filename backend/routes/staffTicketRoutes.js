9deb2f80c73cbe26c62d7e91ace7622875628029
rennleon <renleonz@gmail.com>
2022-08-20 16:38:43 -0500

    Fix scope for middleware

--- backend/routes/staffTicketRoutes.js
+++ backend/routes/staffTicketRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const { verifyStaffId } = require("../middleware/verifyStaffId");
 const controller = require("../controllers/staffTicketsController");
 
-router.use(verifyStaffId);
-
-router.get("/:staffId/tickets", controller.geSttaffTickets);
-router.get("/:staffId/tickets/:id/take", controller.takeTicket);
-router.get("/:staffId/tickets/:id/close", controller.closeTicket);
+router.get("/:staffId/tickets", verifyStaffId, controller.geSttaffTickets);
+router.get("/:staffId/tickets/:id/take", verifyStaffId, controller.takeTicket);
+router.get(
+  "/:staffId/tickets/:id/close",
+  verifyStaffId,
+  controller.closeTicket
+);
 
 module.exports = router;
