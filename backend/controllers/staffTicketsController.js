d9c6a592f77679fd5c89b7e84f6429e31a6987c8
rennleon <renleonz@gmail.com>
2022-08-20 15:58:35 -0500

    Create controller for staff tickets

---
+++ backend/controllers/staffTicketsController.js
@@ -0,0 +1,44 @@
+const { request, response } = require("express");
+const Ticket = require("../models/Ticket");
+const TICKET_STATUS = require("../config/ticketStatusConstants");
+
+const geSttaffTickets = async (req = request, res = response) => {
+  const tickets = await Ticket.find({ staffId: req.userId }).exec();
+  if (!tickets || tickets.length === 0) return res.sendStatus(204);
+
+  res.json(tickets);
+};
+
+const takeTicket = async (req = request, res = response) => {
+  const foundTicket = await Ticket.findOne({
+    _id: req.params.id,
+    userId: req.userId,
+  }).exec();
+  if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
+
+  foundTicket.staffId = userId;
+  foundTicket.status = TICKET_STATUS.ASSIGNED;
+  await foundTicket.save();
+
+  res.sendStatus(200);
+};
+
+const closeTicket = async (req = request, res = response) => {
+  const foundTicket = await Ticket.findOne({
+    _id: req.params.id,
+    userId: req.userId,
+  }).exec();
+  if (!foundTicket) return res.status(404).json({ message: "404 Not Found" });
+
+  foundTicket.status = TICKET_STATUS.CLOSED;
+  foundTicket.closedAt = new Date().toISOString();
+  await foundTicket.save();
+
+  res.json(ticket);
+};
+
+module.exports = {
+  geSttaffTickets,
+  takeTicket,
+  closeTicket,
+};
