c7943583b7448ba5dd93a37b0ec1f0d26340a527
rennleon <renleonz@gmail.com>
2022-08-20 11:22:51 -0500

    Add cors to server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
-const server = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 
+const corsOptions = require("./config/corsOptions");
+
+const server = express();
+
 const PORT = process.env.PORT || 8080;
+
+// Setup cors
+server.use(cors(corsOptions));
 
 mongoose.connect(process.env.DB_CONN_STR, () => {
   console.log("Connection to DB stablished");
