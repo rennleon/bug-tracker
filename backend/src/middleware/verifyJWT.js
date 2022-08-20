9dff84d47ccbb7cd3605ac5323156d3e28472b70
rennleon <renleonz@gmail.com>
2022-08-20 18:38:09 -0500

    Create middleware to handle token verification

---
+++ backend/src/middleware/verifyJWT.js
@@ -0,0 +1,25 @@
+const { request, response } = require("express");
+const { verifyAccessToken } = require("../utils/tokens");
+
+const verifyJWT = async (req = request, res = response, next) => {
+  const authHeaders = req.headers?.authorization || !req.headers?.Authorization;
+  if (!authHeaders?.startsWith("Bearer ")) return res.sendStatus(401);
+
+  try {
+    const accessToken = authHeaders.split(" ")[1];
+    const { user: userId, roles } = await verifyAccessToken(accessToken);
+
+    // inject token info in request
+    req.user = {
+      id: userId,
+      roles,
+    };
+
+    next();
+  } catch (error) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
+module.exports = verifyJWT;
