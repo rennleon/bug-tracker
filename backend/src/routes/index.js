b6926670d4c62129473f53795ab823db938b7945
rennleon <renleonz@gmail.com>
2022-08-20 19:25:42 -0500

    Create route for refresh token

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,18 +4,22 @@ const router = express.Router();
 const verifyJWT = require("../middleware/verifyJWT");
 
 const loginRoutes = require("./loginRoutes");
+const refreshTokenRoutes = require("./refreshTokenRoutes");
 
 const userRoutes = require("./userRoutes");
 const ticketRoutes = require("./ticketRoutes");
 const userTicketRoutes = require("./userTicketRoutes");
 const staffTicketRoutes = require("./staffTicketRoutes");
 
+// Public routes
 router.use("/login", loginRoutes);
+router.use("/refresh-token", refreshTokenRoutes);
 
+// Auth middleware
 router.use(verifyJWT);
 
+// Protected routes
 // PENDING: VERIFY ROLES ...
-
 router.use("/users", userRoutes);
 router.use("/users", userTicketRoutes);
 router.use("/staff", staffTicketRoutes);
---
+++ backend/src/routes/refreshTokenRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controllers/refreshTokenController");
+
+router.get("/", controller.handleRefreshToken);
+
+module.exports = router;
