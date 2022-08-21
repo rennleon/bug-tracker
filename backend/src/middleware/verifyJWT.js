b5a5160959b4ecc0890f6468f4cb4f3e8214ebca
rennleon <renleonz@gmail.com>
2022-08-20 21:03:28 -0500

    Improve token verification

--- backend/src/middleware/verifyJWT.js
+++ backend/src/middleware/verifyJWT.js
@@ -1,13 +1,21 @@
 const { request, response } = require("express");
-const { verifyAccessToken } = require("../utils/tokens");
+const JWT_REFRESH_COOKIE = require("../config/jwtCookie");
+const { verifyAccessToken, verifyRefreshToken } = require("../utils/tokens");
 
 const verifyJWT = async (req = request, res = response, next) => {
+  const cookies = req.cookies;
+  if (!cookies || !cookies[JWT_REFRESH_COOKIE.name]) return res.sendStatus(401);
+
   const authHeaders = req.headers?.authorization || req.headers?.Authorization;
   if (!authHeaders?.startsWith("Bearer ")) return res.sendStatus(401);
 
   try {
     const accessToken = authHeaders.split(" ")[1];
-    const { user: userId, roles } = await verifyAccessToken(accessToken);
+    const refreshToken = cookies[JWT_REFRESH_COOKIE.name];
+    const { user, roles } = await verifyAccessToken(accessToken);
+    const { user: userId } = await verifyRefreshToken(refreshToken);
+
+    if (user !== userId) return res.sendStatus(401);
 
     // inject token info in request
     req.user = {
