8478330b288796bae7ddb282c9c56427b8e53c66
rennleon <renleonz@gmail.com>
2022-08-20 21:36:20 -0500

    Add route to get ticket by id

--- backend/src/routes/staffTicketRoutes.js
+++ backend/src/routes/staffTicketRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/staffTicketsController");
 
 router.get("/my-tickets", controller.geSttaffTickets);
+router.get("/my-tickets/:id", controller.geSttaffTicketById);
 router.get("/my-tickets/:id/take", controller.takeTicket);
 router.get("/my-tickets/:id/close", controller.closeTicket);
 
