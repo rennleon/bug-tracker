7a9b43d7d9dd5c13be93738fd5168a2bede2e4de
rennleon <renleonz@gmail.com>
2022-08-20 19:37:13 -0500

    Refactor refresh controller

--- backend/src/controllers/refreshTokenController.js
+++ backend/src/controllers/refreshTokenController.js
@@ -1,13 +1,14 @@
 const { request, response } = require("express");
+const JWT_REFRESH_COOKIE = require("../config/jwtCookie");
 const User = require("../models/User");
 const { generateAccessToken, verifyRefreshToken } = require("../utils/tokens");
 
 const handleRefreshToken = async (req = request, res = response) => {
   const cookies = req.cookies;
-  if (!cookies?.jwt) return res.sendStatus(401);
+  if (!cookies || !cookies[JWT_REFRESH_COOKIE.name]) return res.sendStatus(401);
 
   try {
-    const refreshToken = cookies.jwt;
+    const refreshToken = cookies[JWT_REFRESH_COOKIE.name];
     const { user: userId } = await verifyRefreshToken(refreshToken);
 
     const foundUser = await User.findOne({ refreshToken }).exec();
