7bf21356d2fb110c6a28d304b96e459a93101336
rennleon <renleonz@gmail.com>
2022-08-23 22:15:10 -0500

    Add route to take list available tickets for staff to take

--- backend/src/routes/staffTicketRoutes.js
+++ backend/src/routes/staffTicketRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/staffTicketsController");
 
 router.get("/", controller.geSttaffTickets);
+router.get("/untaken", controller.listUntakenTickets);
 router.get("/:id", controller.geSttaffTicketById);
 router.get("/:id/take", controller.takeTicket);
 router.get("/:id/close", controller.closeTicket);
