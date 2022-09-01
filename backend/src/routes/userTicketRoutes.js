e76d48c88e56b8a63ee7318ee9ec063859b23e50
rennleon <renleonz@gmail.com>
2022-09-01 18:46:13 -0500

    Add middleware validations to user tickets

--- backend/src/routes/userTicketRoutes.js
+++ backend/src/routes/userTicketRoutes.js
@@ -2,16 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/userTicketsController");
+const {
+  ticketValidator,
+} = require("../middleware/validators/ticketValidators");
 
 router
   .route("/")
   .get(controller.getUserTickets)
-  .post(controller.createUserTicket);
+  .post(ticketValidator, controller.createUserTicket);
 
 router
   .route("/:id")
   .get(controller.getUserTicketById)
-  .put(controller.updateUserTicket)
+  .put(ticketValidator, controller.updateUserTicket)
   .delete(controller.deleteUserTicket);
 
 module.exports = router;
